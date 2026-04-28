aba4e9fd54ff683fe9f106cfe4c8ab76cc00add3
Vrab1ca <niikoola08@gmail.com>
2026-04-28 22:53:35 +0300

    change , add crete map  for member for name , last name , email , orders , orders , customerId

--- Flower Shop/Mapping/AutoMapperProfile.cs
+++ Flower Shop/Mapping/AutoMapperProfile.cs
@@ -1,7 +1,6 @@
-﻿using AutoMapper;
+using AutoMapper;
 using FlowerShopOnlineOrderSystem.Models;
 using FlowerShopOnlineOrderSystem.ViewModels;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FlowerShopOnlineOrderSystem.Mapping
 {
@@ -10,6 +9,17 @@ namespace FlowerShopOnlineOrderSystem.Mapping
         public AutoMapperProfile()
         {
             CreateMap<Flower, FlowerViewModel>().ReverseMap();
+
+            CreateMap<Flower, OrderItemInputViewModel>()
+                .ForMember(dest => dest.FlowerName, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Quantity, opt => opt.Ignore());
+
+            CreateMap<OrderCreateViewModel, Customer>()
+                .ForMember(dest => dest.CustomerId, opt => opt.Ignore())
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.CustomerFirstName.Trim()))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.CustomerLastName.Trim()))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.CustomerEmail.Trim().ToLowerInvariant()))
+                .ForMember(dest => dest.Orders, opt => opt.Ignore());
 
             CreateMap<OrderItem, OrderItemViewModel>()
                 .ForMember(dest => dest.FlowerName,
@@ -23,8 +33,19 @@ namespace FlowerShopOnlineOrderSystem.Mapping
                         src.Customer != null
                             ? src.Customer.FirstName + " " + src.Customer.LastName
                             : string.Empty))
+                .ForMember(dest => dest.CustomerEmail,
+                    opt => opt.MapFrom(src => src.Customer != null ? src.Customer.Email : string.Empty))
                 .ForMember(dest => dest.Items,
                     opt => opt.MapFrom(src => src.OrderItems));
+
+            CreateMap<Order, OrderListViewModel>()
+                .ForMember(dest => dest.CustomerName,
+                    opt => opt.MapFrom(src =>
+                        src.Customer != null
+                            ? src.Customer.FirstName + " " + src.Customer.LastName
+                            : string.Empty))
+                .ForMember(dest => dest.CustomerEmail,
+                    opt => opt.MapFrom(src => src.Customer != null ? src.Customer.Email : string.Empty));
         }
     }
 }
